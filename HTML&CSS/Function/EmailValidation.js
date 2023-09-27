b47d00ff67638bf163c6d0c5796a22430b87dc53
sumit sharma <sumitkumarsharma2748@gmail.com>
2023-09-27 14:04:38 +0530

    hell

---
+++ HTML&CSS/Function/EmailValidation.js
@@ -0,0 +1,17 @@
+
+const email = prompt('enter the email address')
+const emailLen = email.length;
+const dotIndex = email.lastIndexOf('.');
+const atIndex = email.lastIndexOf('@');
+const lastIndex = emailLen-1;
+
+if(
+emailLen<11 && 
+lastIndex - dotIndex < 2 &&
+ lastIndex- dotIndex>3 &&
+ dotIndex-atIndex<3
+){
+    console.log("Invelid email")
+}else{
+    console.log("Valid email");
+};
--- HTML&CSS/Javascripts/EmailValidation.js
+++ HTML&CSS/Javascripts/EmailValidation.js
@@ -19,9 +19,9 @@ const atIndex = email.lastIndexOf('@');
 const lastIndex = emailLen-1;
 
 if(
-emailLen<11 || 
-lastIndex - dotIndex < 2 ||
- lastIndex- dotIndex>3||
+emailLen<11 && 
+lastIndex - dotIndex < 2 &&
+ lastIndex- dotIndex>3 &&
  dotIndex-atIndex<3
 ){
     console.log("Invelid email")
