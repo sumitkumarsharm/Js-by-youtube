5a23ecc730995b48e7591d19ac306d783a6706c3
sumit sharma <sumitkumarsharma2748@gmail.com>
2023-10-05 13:03:52 +0530

    java scripts

---
+++ Chai aur code/01_constVarLet.js
@@ -0,0 +1,18 @@
+const accountId = 123456;
+
+let accountGmail = "sumit@google.com";
+var accountPassword = "1234567";
+accountCity = "bhopal";
+// accountId = 123  // not allow
+accountGmail = "hc@hc.com";
+accountPassword = '1234rfgbn';
+accountCity = "bihar";
+
+let accountdetail;
+/*prefer not use to var
+bacouse of issue in block code in block scope and functional scope
+
+ */
+
+ 
+ console.table([accountId ,accountGmail, accountPassword, accountCity, accountdetail]);
---
+++ Chai aur code/02_dataType.js
@@ -0,0 +1,12 @@
+"use strict";//treat all JS code as newer vesional
+//alert(2+2) //we are using node js not browser
+//code readbility is high
+//number => 2 to the power 53
+//bigInt
+//string
+// boolean
+//null
+// undefined
+// Symbol
+//objects
+
